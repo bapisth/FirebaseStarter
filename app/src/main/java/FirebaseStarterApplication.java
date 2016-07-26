922e10eb96921f5710c994aafeca148c0ac3dab1
bapisth <bapi.bluestack@gmail.com>
2016-07-26 17:27:41 +0530

    Initial commit

---
+++ app/src/main/java/FirebaseStarterApplication.java
@@ -0,0 +1,11 @@
+import android.app.Application;
+
+/**
+ * Created by hemendra on 26-07-2016.
+ */
+public class FirebaseStarterApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
